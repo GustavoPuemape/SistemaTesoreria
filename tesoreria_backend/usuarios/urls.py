f6c50e202b05ca596ba355bde40b02f1e7931118
Isrrael18 <adalbertolano@gmail.com>
2025-05-19 15:42:28 -0500

    se modificó las view de tesoreria

--- tesoreria_backend/usuarios/urls.py
+++ tesoreria_backend/usuarios/urls.py
@@ -4,4 +4,5 @@ from .views import RegistroUsuarioView, CustomTokenObtainPairView
 urlpatterns = [
     path('registro/', RegistroUsuarioView.as_view(), name='registro'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    
 ]
